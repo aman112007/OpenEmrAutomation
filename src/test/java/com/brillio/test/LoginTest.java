23d8571c80f1b4b0edce920e63c09ebc4312cb5d
aman.aman <aman.aman@BRILAP1776.BRILLIO.COM>
2022-05-31 13:02:19 +0530

    added dataprovider and connected with test

--- src/test/java/com/brillio/test/LoginTest.java
+++ src/test/java/com/brillio/test/LoginTest.java
@@ -1,24 +1,45 @@
 package com.brillio.test;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.brillio.base.WebDriverWrapper;
 
 public class LoginTest extends WebDriverWrapper {
 
-	@Test
-	public void validCredentialTest() {
+	@DataProvider
+	public String[][] validCredentialData() {
+		String[][] main = new String[2][4];
 
-		driver.findElement(By.id("authUser")).sendKeys("admin");
-		driver.findElement(By.id("clearPass")).sendKeys("pass");
+		main[0][0] = "admin";
+		main[0][1] = "pass";
+		main[0][2] = "English (Indian)";
+		main[0][3] = "OpenEMR";
+		main[1][0] = "physician";
+		main[1][1] = "physician";
+		main[1][2] = "Dutch";
+		main[1][3] = "OpenEMR";
+
+		return main;
+
+	}
+
+	@Test(dataProvider = "validCredentialData")
+	public void validCredentialTest(String username, String password, String language, String expectedTitle) {
+
+		driver.findElement(By.id("authUser")).sendKeys(username);
+		driver.findElement(By.id("clearPass")).sendKeys(password);
+
+		Select selectLan = new Select(driver.findElement(By.xpath("//select[@name='languageChoice']")));
+		selectLan.selectByVisibleText(language);
 
 		driver.findElement(By.id("login-button")).click();
 
 		String actualTitle = driver.getTitle();
-
-		Assert.assertEquals(actualTitle, "OpenEMR");
+		Assert.assertEquals(actualTitle, expectedTitle);
 
 	}
 
