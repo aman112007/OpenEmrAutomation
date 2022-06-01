bed4ba7e86be72fcbbf99f8cb150993528d5558b
aman.aman <aman.aman@BRILAP1776.BRILLIO.COM>
2022-06-01 12:41:05 +0530

    valid credential with excel connect

--- src/test/java/com/brillio/utilities/DataUtils.java
+++ src/test/java/com/brillio/utilities/DataUtils.java
@@ -6,22 +6,7 @@ import org.testng.annotations.DataProvider;
 
 public class DataUtils {
 
-	@DataProvider
-	public String[][] validCredentialData() {
-		String[][] main = new String[2][4];
-
-		main[0][0] = "admin";
-		main[0][1] = "pass";
-		main[0][2] = "English (Indian)";
-		main[0][3] = "OpenEMR";
-		main[1][0] = "physician";
-		main[1][1] = "physician";
-		main[1][2] = "Dutch";
-		main[1][3] = "OpenEMR";
-
-		return main;
-
-	}
+	
 
 	@DataProvider
 	public Object[][] invalidCredentialData() throws IOException {
@@ -31,5 +16,30 @@ public class DataUtils {
 		return main;
 
 	}
+	
+	@DataProvider
+	public Object[][] validCredentialData() throws IOException {
 
+		Object[][] main = ExcelUtils.getSheetIntoTwoDimArray("test-data/orange_data.xlsx", "validCredentialTest");
+
+		return main;
+
+	}
+
+	//@DataProvider
+	//public String[][] validCredentialData() {
+		//String[][] main = new String[2][4];
+
+		//main[0][0] = "admin";
+		//main[0][1] = "pass";
+		//main[0][2] = "English (Indian)";
+		//main[0][3] = "OpenEMR";
+		//main[1][0] = "physician";
+		//main[1][1] = "physician";
+		//main[1][2] = "Dutch";
+		//main[1][3] = "OpenEMR";
+
+		//return main;
+
+	//}
 }
